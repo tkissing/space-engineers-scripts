1e3a55ede2468693501ca6bd1bfea6b7528fc740
Timo Van-Kissing <timo.kissing@gmail.com>
2024-09-09 15:33:09 -0700

    Rotate between components and ingot counts every 30 seconds

--- ingot-counter/Program.cs
+++ ingot-counter/Program.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using VRage;
 using VRage.Collections;
 using VRage.Game;
@@ -41,11 +42,12 @@ namespace IngameScript
         public void Main(string argument, UpdateType updateSource)
         {
             List<IMyCargoContainer> blocks = new List<IMyCargoContainer>();
+
+            SortedDictionary<string, SortedDictionary<string, MyFixedPoint>> counts = new SortedDictionary<string, SortedDictionary<string, MyFixedPoint>>();
+
+            string[] categories = new string[] { "Ingot", "Component" };
+
             SortedDictionary<string, MyFixedPoint> ingotCounts = new SortedDictionary<string, MyFixedPoint>();
-
-            List<string> lines = new List<string>();
-
-            lines.Add("Current Inventory");
 
             GridTerminalSystem.GetBlocksOfType(blocks, block => block.IsSameConstructAs(Me) && block is IMyCargoContainer);
 
@@ -59,43 +61,64 @@ namespace IngameScript
                 {
                     string itemName = item.Type.ToString();
 
-                    if (itemName.Contains("Ingot"))
+                    foreach (string category in categories)
                     {
-                        string metal = (string)item.Type.SubtypeId;
-
-                        if (ingotCounts.ContainsKey(metal))
+                        if (itemName.Contains($"_{category}/"))
                         {
+                            if (!counts.ContainsKey(category))
+                            {
+                                counts[category] = new SortedDictionary<string, MyFixedPoint>();
+                            }
 
-                            ingotCounts[metal] += item.Amount;
-                        }
-                        else
-                        {
-                            ingotCounts[metal] = item.Amount;
+                            string subtype = (string)item.Type.SubtypeId;
+
+                            if (counts[category].ContainsKey(subtype))
+                            {
+
+                                counts[category][subtype] += item.Amount;
+                            }
+                            else
+                            {
+                                counts[category][subtype] = item.Amount;
+                            }
                         }
                     }
-
                 }
             }
 
-            foreach (var itemName in ingotCounts.Keys)
+            int secondsPerCategory = (int)(60 / categories.Count());
+
+            string categoryToShow = categories[(int)Math.Floor((double)(DateTime.Now.Second / secondsPerCategory))];
+
+            var counterToShow = counts[categoryToShow];
+
+            List<string> lines = new List<string>();
+
+            lines.Add($"Current {categoryToShow} Inventory");
+
+            foreach (var itemName in counterToShow.Keys)
             {
                 var parts = itemName.Split('/');
 
                 if (parts.Length > 0)
                 {
-                    var amount = ToSI(ingotCounts[itemName]);
+                    var amount = ToSI(counterToShow[itemName]);
                     lines.Add($"{parts[parts.Length - 1]}: {amount}");
                 }
             }
 
-            string text = String.Join("\n", lines.ToArray());
-            Show(text);
+            Show(lines);
         }
 
-        private void Show(string text)
+        private void Show(List<string> lines)
         {
-            var match = (new System.Text.RegularExpressions.Regex(" SHOW:([^ ]+)")).Match(Me.DisplayNameText);
+            float fontSize = 17.0f / lines.Count();
 
+            string debug = $"DEBUG {lines.Count} {fontSize}";
+
+            string text = String.Join("\n", lines.ToArray());
+
+            var match = (new System.Text.RegularExpressions.Regex(" SHOW:([^ ]+)")).Match(Me.DisplayNameText);
 
             List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
             if (match.Success)
@@ -108,10 +131,11 @@ namespace IngameScript
 
             foreach (var block in blocks)
             {
+                (block as IMyTextSurfaceProvider).GetSurface(0).FontSize = fontSize;
                 (block as IMyTextSurfaceProvider).GetSurface(0).WriteText(text);
             }
 
-            Echo(text);
+            Echo($"{text}\n{debug}");
         }
 
         private string ToSI(MyFixedPoint a, string format = "f2")
